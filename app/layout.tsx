f9944a1b3a00c58df466637c00252fb3e3574455
Alfie <rapidclearsolutions26@gmail.com>
2026-09-08 17:09:34 +0100

    Add analytics and mobile app support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import type {
-  Metadata,
-  Viewport,
-} from "next";
+import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -55,7 +53,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
